83faa84c2a5f56007544faa9fbf068f40581b93a
Nataly Gonzalez Ornelas <ngonzalezornelas@ucsb.edu>
2025-10-07 17:48:16 -0700

    xy - added tests for equals to TeamTest

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -11,7 +11,7 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");
     }
 
     @Test
@@ -19,8 +19,88 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    // Case 1: Same object
+    @Test
+    public void equals_returns_same_Object() {
+        Team tmp = team;
+
+        assert(team.equals(tmp));
+    }
+
+    // Case 2: Object is not a Team object; different type (i.e., int, String)
+    @Test
+    public void object_not_instance_Team() {
+        int teamNumber = 13;
+
+        assertEquals(false, team.equals(teamNumber));
+    }
+
+    // Case 3: Comparing fields
+    @Test
+    public void returns_true_same_name() {
+        Team tmp = new Team("test-team");
+
+        assert(team.name.equals(tmp.name));
+    }
+
+    @Test
+    public void returns_true_same_members() {
+        Team tmp1 = new Team("tmp1");
+        tmp1.addMember("Member 1");
+        Team tmp2 = new Team("tmp2");
+        tmp2.addMember("Member 1");
+
+        assert(tmp1.members.equals(tmp2.members));
+    }
+
+    @Test
+    public void returns_true_same_values() {
+        Team tmp = new Team("test-team");
+        tmp.addMember("Member 1");
+
+        team.addMember("Member 1");
+
+        assert(team.equals(tmp));
+    }
+
+    @Test
+    public void returns_false_different_name() {
+        Team tmp = new Team("tmp");
+
+        assertEquals(false, team.name.equals(tmp.name));
+    }
+
+    @Test
+    public void returns_false_different_members() {
+        Team tmp = new Team("test-team");
+        tmp.addMember("Member 1");
+
+        assertEquals(false, team.members.equals(tmp.members));
+    }
+
+    @Test
+    public void returns_false_different_values() {
+        Team tmp1 = new Team("tmp1");
+        Team tmp2 = new Team("tmp2");
+
+        tmp1.addMember("Member 1");
+        tmp2.addMember("Member 2");
+
+        assertEquals(false, tmp1.equals(tmp2));
+    }
+
+    // Hash code check
+    @Test
+    public void check_hash_Code() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 
 }
